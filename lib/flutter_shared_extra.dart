3348ec59b4acd114ab5eebdd04afae498d3171b4
Steve Gehrman <sgehrman@gmail.com>
2020-05-22 21:08:56 -0700

    fixes

--- lib/flutter_shared_extra.dart
+++ lib/flutter_shared_extra.dart
@@ -12,7 +12,6 @@ export 'src/firebase/serializable.dart';
 export 'src/image/image_url_model.dart';
 export 'src/image/image_delete_dialog.dart';
 export 'src/image/image_upload_dialog.dart';
-export 'src/image/super_image.dart';
 
 export 'src/login/user_login_screen.dart';
 export 'src/login/phone_verifier.dart';
